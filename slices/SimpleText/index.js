121440fff80833c3870ab1d0e5364b9cf60b665d
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-05 13:53:08 +0000

    set up basic layout

--- slices/SimpleText/index.js
+++ slices/SimpleText/index.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import { array, shape } from 'prop-types';
+import { richTextPropType } from '../../helpers/slice-prop-types';
 import { RichText } from 'prismic-reactjs';
-
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
-
-const h2 = {
-  color: '#8592e0',
-};
+import { FlexColumn } from '../../components/containers';
+import { htmlSerializer } from '../../prismicKits';
 
 const MySlice = ({ slice }) => (
-  <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
-  </section>
+  <FlexColumn>
+    <RichText render={slice.primary.text} htmlSerializer={htmlSerializer}/>
+  </FlexColumn>
 );
 
 MySlice.propTypes = {
   slice: shape({
     primary: shape({
-      title: array.isRequired,
+      text: richTextPropType.isRequired,
     }).isRequired,
   }).isRequired,
 };

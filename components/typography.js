af38587a6e18aa29453e370590322adc39a99edd
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-05 12:00:30 +0000

    created Typography base components

---
+++ components/typography.js
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const Header = styled.h1`
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${({ fontSize, theme: { fontSizes }}) => fontSize ? fontSizes[fontSize] : fontSizes.xl };
+  line-height: ${props => props.theme.lineHeights.heading};
+  color: ${({ color, theme: { colors } }) => color ? color : colors.heading };
+`
+
+export const Body = styled.p`
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${({ fontSize, theme: { fontSizes }}) => fontSize ? fontSizes[fontSize] : fontSizes.sm };
+  line-height: ${props => props.theme.lineHeights.body};
+  color: ${({ color, theme: { colors } }) => color ? color : colors.body };
+`

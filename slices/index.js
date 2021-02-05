a8ace862fe08a11c4d8013815a94add8712341a3
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-05 17:14:08 +0000

    created FaqSection

--- slices/index.js
+++ slices/index.js
@@ -1,3 +1,4 @@
 export { default as SimpleText } from './SimpleText'
 export { default as CompanyLogos } from './CompanyLogos'
 export { default as DisclaimerBanner } from './DisclaimerBanner'
+export { default as FaqSection } from './FaqSection'

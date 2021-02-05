f26ba8b0cd4294d8c1244dd3a2def9e1ae775f46
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-05 12:01:00 +0000

    created styled-components theme

---
+++ styles/theme.js
@@ -0,0 +1,38 @@
+export default theme = {
+  colors: {
+    body: '#070611',
+    heading: '#B80F11',
+    primary: '#D11013',
+    secondary: '#059638',
+    muted: '#F9BDBE',
+    background: '#EFF0F2',
+  },
+  fonts: {
+    body: '"Source Sans Pro", sans-serif',
+    heading: '"Bebas Neue", sans-serif',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '1rem',
+    md: '1.25rem',
+    lg: '1.5rem',
+    xl: '2rem',
+  },
+  fontWeights: {
+    thin: 100,
+    body: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.25,
+    heading: 1.125,
+  },
+  spacings: {
+    none: 0,
+    xs: '0.25rem',
+    sm: '0.5rem',
+    md: '1rem',
+    lg: '2rem',
+    xl: '4rem',
+  }
+}

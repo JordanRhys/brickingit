a02d172bede3f81553298e994b61be2146c4921e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-07 11:32:48 +0000

    styled FaqSection

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
+import GlobalStyle from '../styles/globals';
 import { addDecorator } from '@storybook/react';
 
 const ThemeDecorator = storyFn => (
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     {storyFn()}
   </ThemeProvider>
 )

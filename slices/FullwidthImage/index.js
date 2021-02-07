0e1b43d256bd13161895cea6c002d45f0298e90d
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-07 13:45:58 +0000

    styled FullwidthImage

--- slices/FullwidthImage/index.js
+++ slices/FullwidthImage/index.js
@@ -1,36 +1,24 @@
 import React from 'react';
-import { array, shape } from 'prop-types';
-import { RichText } from 'prismic-reactjs';
+import { shape, string, } from 'prop-types';
+import { imagePropType } from '../../helpers/slice-prop-types';
+import { FlexColumn } from '../../components/containers';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
-
-const h2 = {
-  color: '#8592e0',
-};
 
 const MySlice = ({ slice }) => (
-  <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
-  </section>
+  <FlexColumn>
+    <img src={slice.primary.image.url} alt={slice.primary.image.alt}/>
+
+    { slice.primary.caption && (
+      <span>{slice.primary.caption}</span>
+    )}
+  </FlexColumn>
 );
 
 MySlice.propTypes = {
   slice: shape({
     primary: shape({
-      title: array.isRequired,
+      image: imagePropType.isRequired,
+      caption: string
     }).isRequired,
   }).isRequired,
 };

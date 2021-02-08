f290dab13e429337cba5bf97cb3866f0c1fa004e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-08 18:36:08 +0000

    styled MainFooter

--- slices/MainFooter/index.js
+++ slices/MainFooter/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import { array, shape } from 'prop-types';
+import { shape, arrayOf } from 'prop-types';
+import { htmlSerializer } from '../../prismicKits';
+import { richTextPropType } from '../../helpers/slice-prop-types';
+import { FlexColumn, FlexRow } from '../../components/containers';
 import { RichText } from 'prismic-reactjs';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
-
-const h2 = {
-  color: '#8592e0',
-};
-
 const MySlice = ({ slice }) => (
-  <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
-  </section>
+  <FlexColumn>
+    <FlexRow withoutPadding>
+      {
+        slice.items.map(({ column }, index) => (
+          <FlexColumn key={index} withoutPadding>
+            <RichText render={column} htmlSerializer={htmlSerializer} />
+          </FlexColumn>
+        ))
+      }
+    </FlexRow>
+    <RichText render={slice.primary.legal} htmlSerializer={htmlSerializer} />
+  </FlexColumn>
 );
 
 MySlice.propTypes = {
   slice: shape({
     primary: shape({
-      title: array.isRequired,
+      legal: richTextPropType.isRequired,
     }).isRequired,
+    items: arrayOf(shape({
+      column: richTextPropType.isRequired
+    })).isRequired,
   }).isRequired,
 };
 

1da45590332390773fecd975c3542377d7c25387
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-09 09:38:50 +0000

    createdfunction to convert pence to pounds

---
+++ helpers/currency.js
@@ -0,0 +1,19 @@
+export const penceToPounds = (price) => {
+  if (!price) return;
+
+  var string = price;
+
+  if (typeof string !== 'string') {
+    string = string.toString()
+  }
+
+  string = string.split('');
+
+  if (string.length > 2) {
+    var start = string.slice(0, string.length - 2);
+    var end = string.slice(string.length - 2, string.length);
+    return `£${start.join('')}.${end.join('')}`;
+  }
+
+  return '£0.' + string;
+}

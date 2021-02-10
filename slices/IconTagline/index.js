9cae9c8c68aaf542f2a71d297b6d4fff81845c72
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-10 14:52:43 +0000

    styled IconTagline

--- slices/IconTagline/index.js
+++ slices/IconTagline/index.js
@@ -1,37 +1,58 @@
-import React from 'react';
-import { array, shape } from 'prop-types';
+import React, { useState } from 'react';
+import { shape, arrayOf, object, string, bool } from 'prop-types';
+import { imagePropType, richTextPropType } from '../../helpers/slice-prop-types';
+import { penceToPounds } from '../../helpers/currency';
+import { FlexColumn, FlexRow, Card, FullWidth } from '../../components/containers';
+import { htmlSerializer } from '../../prismicKits';
+import { PrimaryButton, SecondaryButton } from '../../components/buttons';
 import { RichText } from 'prismic-reactjs';
+import styled from 'styled-components';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
+const Column = styled.div`
+  max-width: 50%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  > *:not(:last-child) {
+    margin-bottom: ${props => props.theme.spacings.md};
+  }
+`
 
-const h2 = {
-  color: '#8592e0',
-};
+const Icon = styled.img`
+  width: 3rem;
+  height: 3rem;
+`
+
+const Tagline = styled.span`
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${props => props.theme.fontSizes.md};
+  font-weight: ${props => props.theme.fontWeights.heading};
+  line-height: ${props => props.theme.lineHeights.heading};
+  color: ${props => props.theme.colors.primary};
+`
 
 const MySlice = ({ slice }) => (
-  <section style={section}>
+  <FlexRow>
     {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
+      slice.items.map(({ icon, tagline, text }, index) => (
+        <Column key={index}>
+          <Icon src={icon.url} alt={icon.alt} />
+          <Tagline>{tagline}</Tagline>
+          <RichText render={text} htmlSerializer={htmlSerializer} />
+        </Column>
+      ))
     }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
-  </section>
+  </FlexRow>
 );
 
 MySlice.propTypes = {
   slice: shape({
-    primary: shape({
-      title: array.isRequired,
-    }).isRequired,
+    primary: object.isRequired,
+    items: arrayOf(shape({
+      icon: imagePropType.isRequired,
+      tagline: string.isRequired,
+      text: richTextPropType.isRequired,
+    })).isRequired,
   }).isRequired,
 };
 

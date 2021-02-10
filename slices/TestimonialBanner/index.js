91fe3eccaee6916236dbfc937c1408fcd97e3d87
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-10 09:39:44 +0000

    styled TestimonialBanner

--- slices/TestimonialBanner/index.js
+++ slices/TestimonialBanner/index.js
@@ -1,37 +1,80 @@
-import React from 'react';
-import { array, shape } from 'prop-types';
+import React, { useState } from 'react';
+import { shape, arrayOf, object, string, number } from 'prop-types';
+import { imagePropType, richTextPropType } from '../../helpers/slice-prop-types';
+import { penceToPounds } from '../../helpers/currency';
+import { FlexColumn, FlexRow, Card, FullWidth } from '../../components/containers';
+import { htmlSerializer } from '../../prismicKits';
+import { PrimaryButton, SmallPrimaryButton, SmallSecondaryButton } from '../../components/buttons';
 import { RichText } from 'prismic-reactjs';
+import styled from 'styled-components';
+import Slider from 'infinite-react-carousel';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
+const Background = styled.section`
+  background-color: ${props => props.theme.colors.primary}
+`
+
+const HalfWidth = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  object-fit: cover;
+  color: ${props => props.theme.colors.background};
+  ${({ padding, theme: { spacings }}) => padding ? `padding: ${spacings.md} ${spacings.lg};` : ''}
+  > * {
+    padding-left: ${props => props.theme.spacings.sm};
+  }
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  opacity: 0.75;
+`
+
+const Name = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: inherit;
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${props => props.theme.fontSizes.lg};
+  line-height: ${props => props.theme.lineHeights.heading};
+`
+
+const MySlice = ({ slice }) => {
+
+  return (
+      <Slider dots>
+        {
+          slice.items.map(({ image, name, text }) => (
+          <Background>
+            <FlexRow withoutPadding withoutMargin>
+              <HalfWidth padding>
+                <RichText render={text} htmlSerializer={htmlSerializer} />
+              </HalfWidth>
+              <HalfWidth>
+                <Image src={image.url} alt={image.alt}/>
+                <Name>{name}</Name>
+              </HalfWidth>
+            </FlexRow>
+          </Background>
+          ))
+        }
+      </Slider>
+  );
 };
-
-const h2 = {
-  color: '#8592e0',
-};
-
-const MySlice = ({ slice }) => (
-  <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
-  </section>
-);
 
 MySlice.propTypes = {
   slice: shape({
-    primary: shape({
-      title: array.isRequired,
-    }).isRequired,
+    primary: object.isRequired,
+    items: arrayOf(shape({
+      image: imagePropType.isRequired,
+      name: string.isRequired,
+      text: richTextPropType.isRequired,
+    })).isRequired
   }).isRequired,
 };
 

27050b9866f6ed3f3c71b3f8bc5be81c3d6f5280
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-13 14:05:22 +0000

    created breakpoints

---
+++ styles/breakpoints.js
@@ -0,0 +1,11 @@
+const sizes = {
+  sm: '400px',
+  md: '720px',
+  lg: '1000px'
+}
+
+export const breakpoints = {
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+}

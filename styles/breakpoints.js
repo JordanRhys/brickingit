02e4b3210909d88cfbccce1ec0c87bc3c7193e3e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-13 15:48:17 +0000

    added mdlg breakpoint

--- styles/breakpoints.js
+++ styles/breakpoints.js
@@ -1,11 +1,13 @@
 const size = {
   sm: '400px',
-  md: '720px',
+  md: '700px',
+  mdlg: '800px',
   lg: '1000px'
 }
 
 export const breakpoints = {
   sm: `(min-width: ${size.sm})`,
   md: `(min-width: ${size.md})`,
+  mdlg: `(min-width: ${size.mdlg})`,
   lg: `(min-width: ${size.lg})`,
 }

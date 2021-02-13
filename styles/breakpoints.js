e9b2efcaeffd235d356c6268eb052be6f9be1921
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-13 16:31:30 +0000

    added smmd small medium breakpoint

--- styles/breakpoints.js
+++ styles/breakpoints.js
@@ -1,5 +1,6 @@
 const size = {
-  sm: '400px',
+  sm: '480px',
+  smmd: '540px',
   md: '700px',
   mdlg: '800px',
   lg: '1000px'
@@ -7,6 +8,7 @@ const size = {
 
 export const breakpoints = {
   sm: `(min-width: ${size.sm})`,
+  smmd: `(min-width: ${size.smmd})`,
   md: `(min-width: ${size.md})`,
   mdlg: `(min-width: ${size.mdlg})`,
   lg: `(min-width: ${size.lg})`,

4f96c5c3ce2298b6d9ac9577192e301e15880f1a
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-13 15:21:30 +0000

    made responsive

--- slices/IconTagline/index.js
+++ slices/IconTagline/index.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import { shape, arrayOf, object, string, bool } from 'prop-types';
+import React from 'react';
+import { shape, arrayOf, object, string } from 'prop-types';
 import { imagePropType, richTextPropType } from '../../helpers/slice-prop-types';
-import { penceToPounds } from '../../helpers/currency';
-import { FlexColumn, FlexRow, Card, FullWidth } from '../../components/containers';
+import { FlexRow } from '../../components/containers';
 import { htmlSerializer } from '../../prismicKits';
-import { PrimaryButton, SecondaryButton } from '../../components/buttons';
 import { RichText } from 'prismic-reactjs';
 import styled from 'styled-components';
+import { breakpoints } from '../../styles/breakpoints';
 
 const Column = styled.div`
-  max-width: 50%;
-  flex: 1;
+  min-width: 200px;
+  max-width: 300px;
+  flex: 1 1;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-bottom: ${props => props.theme.spacings.md};
   > *:not(:last-child) {
-    margin-bottom: ${props => props.theme.spacings.md};
+    margin-bottom: ${props => props.theme.spacings.sm};
+  }
+
+  @media only screen and ${breakpoints.md} {
+    flex: 1;
+    max-width: 50%;
+    align-items: stretch;
+    text-align: left;
+    margin-bottom: 0;
   }
 `
 
@@ -29,6 +40,7 @@ const Tagline = styled.span`
   font-weight: ${props => props.theme.fontWeights.heading};
   line-height: ${props => props.theme.lineHeights.heading};
   color: ${props => props.theme.colors.primary};
+  margin-top: ${props => props.theme.spacings.xs};
 `
 
 const MySlice = ({ slice }) => (

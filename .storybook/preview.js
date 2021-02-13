e038bfd95a872801888dafb68ae7c8ce266d18ca
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-13 14:32:48 +0000

    added Outer component to handle page size

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,13 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import GlobalStyle from '../styles/globals';
 import { addDecorator } from '@storybook/react';
+import { Outer } from '../components/containers';
 
 const ThemeDecorator = storyFn => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <div style={{width: '1000px', margin: '0 auto'}}>
+    <Outer>
       {storyFn()}
-    </div>
+    </Outer>
   </ThemeProvider>
 )
 

69764c39230ecff3e766408098a395458f0acbf7
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-15 12:11:56 +0000

    added theme to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import "../styles/globals.css";
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/globals';
+import theme from '../styles/theme';
+import { Outer } from '../components/containers';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Outer>
+        <Component {...pageProps} />;
+      </Outer>
+    </ThemeProvider>
+  )
 };
 
 export default MyApp;

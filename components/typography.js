9b82da6327edf583c028b2c3f5e942ec471fa96e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-16 14:33:13 +0000

    added font-weights and letter-spacing

--- components/typography.js
+++ components/typography.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Header = styled.h1`
   font-family: ${props => props.theme.fonts.heading};
   font-size: ${({ fontSize, theme: { fontSizes }}) => fontSize ? fontSizes[fontSize] : fontSizes.xl };
+  font-weight: ${props => props.theme.fontWeights.heading};
+  letter-spacing: ${props => props.theme.letterSpacings.heading};
   line-height: ${props => props.theme.lineHeights.heading};
   color: inherit;
   margin: 0;
@@ -11,7 +13,9 @@ export const Header = styled.h1`
 export const Body = styled.p`
   font-family: ${props => props.theme.fonts.body};
   font-size: ${({ fontSize, theme: { fontSizes }}) => fontSize ? fontSizes[fontSize] : fontSizes.sm };
-  line-height: ${props => props.theme.lineHeights.body};
+  font-weight: ${props => props.theme.fontWeights.body};
+  font-spacing: ${props => props.theme.letterSpacings.body};
+  letter-height: ${props => props.theme.lineHeights.body};
   color: inherit;
   margin: 0;
 `

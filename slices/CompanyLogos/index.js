67e31efdfcea702ada49e66fa45ca59dae6bc1cb
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-16 15:43:13 +0000

    added contrast theme option

--- slices/CompanyLogos/index.js
+++ slices/CompanyLogos/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { arrayOf, shape, object } from 'prop-types';
 import { richTextPropType, imagePropType } from '../../helpers/slice-prop-types';
-import { htmlSerializer, linkResolver, hrefResolver } from '../../prismicKits';
+import { htmlSerializer } from '../../prismicKits';
 import { RichText } from 'prismic-reactjs';
-import { FlexColumn, FlexRow } from '../../components/containers';
+import { FlexColumn } from '../../components/containers';
 import { Link } from '../../components/links';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme';
+import contrastTheme from '../../styles/contrastTheme';
 import { breakpoints } from '../../styles/breakpoints';
 
 const Image = styled.img`
-  height: 4rem;
+  max-height: 4rem;
+  max-width: 8rem;
   line-height: 0;
 
   @media only screen and ${breakpoints.md} {
-    height: 4.5rem;
+    max-height: 4.5rem;
+    max-width: 9rem;
   }
 
   @media only screen and ${breakpoints.lg} {
-    height: 5rem;
+    max-height: 5rem;
+    max-width: 10rem;
   }
 `
 
 const Anchor = styled.a`
   line-height: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Wrap = styled.div`
@@ -44,22 +52,24 @@ const Wrap = styled.div`
 `
 
 const MySlice = ({ slice }) => (
-  <FlexColumn>
-    { slice.primary.text && (
-      <RichText render={slice.primary.text} htmlSerializer={htmlSerializer} />
-    )}
-    <Wrap>
-      {slice.items.map(({image, link}) => (
-        link ? (
-          <Link link={link} Component={Anchor}>
-            <Image src={image.url} alt={image.alt} />
-          </Link>
-        ) : (
-          <Image src={image.url} alt={image.alt} key={image.url} />
-        )
-      ))}
-    </Wrap>
-  </FlexColumn>
+  <ThemeProvider theme={slice.primary.contrast ? contrastTheme : theme}>
+    <FlexColumn>
+      { slice.primary.text && (
+        <RichText render={slice.primary.text} htmlSerializer={htmlSerializer} />
+      )}
+      <Wrap>
+        {slice.items.map(({image, link}) => (
+          link ? (
+            <Link link={link} Component={Anchor}>
+              <Image src={image.url} alt={image.alt} />
+            </Link>
+          ) : (
+            <Image src={image.url} alt={image.alt} key={image.url} />
+          )
+        ))}
+      </Wrap>
+    </FlexColumn>
+  </ThemeProvider>
 );
 
 MySlice.propTypes = {

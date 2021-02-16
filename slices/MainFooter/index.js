89ef3d65a5a35f3bc13eddfc1516c8abb01713a3
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-16 15:42:58 +0000

    switched to contrast theme

--- slices/MainFooter/index.js
+++ slices/MainFooter/index.js
@@ -4,11 +4,17 @@ import { htmlSerializer } from '../../prismicKits';
 import { richTextPropType } from '../../helpers/slice-prop-types';
 import { FlexColumn, FlexRow } from '../../components/containers';
 import { RichText } from 'prismic-reactjs';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import contrastTheme from '../../styles/contrastTheme';
 import { breakpoints } from '../../styles/breakpoints';
 
 const MinWidthColumn = styled(FlexColumn)`
   min-width: 200px;
+  text-align: left;
+
+  > * {
+    width: 100%;
+  }
 
   @media only screen and ${breakpoints.md} {
     min-width: 0;
@@ -16,20 +22,22 @@ const MinWidthColumn = styled(FlexColumn)`
 `
 
 const MySlice = ({ slice }) => (
-  <FlexColumn>
-    <FlexRow withoutPadding>
-      {
-        slice.items.map(({ column }, index) => (
-          <MinWidthColumn key={index} withoutPadding>
-            <RichText render={column} htmlSerializer={htmlSerializer} />
-          </MinWidthColumn>
-        ))
-      }
-    </FlexRow>
-    <div style={{ textAlign: 'center' }}>
-      <RichText render={slice.primary.legal} htmlSerializer={htmlSerializer} />
-    </div>
-  </FlexColumn>
+  <ThemeProvider theme={contrastTheme}>
+    <FlexColumn>
+      <FlexRow withoutPadding>
+        {
+          slice.items.map(({ column }, index) => (
+            <MinWidthColumn key={index} withoutPadding>
+              <RichText render={column} htmlSerializer={htmlSerializer} />
+            </MinWidthColumn>
+          ))
+        }
+      </FlexRow>
+      <div style={{ textAlign: 'center' }}>
+        <RichText render={slice.primary.legal} htmlSerializer={htmlSerializer} />
+      </div>
+    </FlexColumn>
+  </ThemeProvider>
 );
 
 MySlice.propTypes = {

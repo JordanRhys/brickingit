c603ec3db19d482ebc93e3428ab0367e34b1c17d
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 20:10:23 +0000

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -62,6 +62,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+          <link rel="shortcut icon" href="/static/icon.ico" />
         </Head>
         <body>
           <Main />

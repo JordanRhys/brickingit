5f51212bdcd19c344d88ecc2f7c7920876aad212
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 12:57:34 +0000

    added padding and made responsive for large screens

--- slices/FormMailing/index.js
+++ slices/FormMailing/index.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { shape, string, func } from 'prop-types';
-import { richTextPropType, imagePropType } from '../../helpers/slice-prop-types';
-import { htmlSerializer, linkResolver } from '../../prismicKits';
+import { richTextPropType } from '../../helpers/slice-prop-types';
+import { htmlSerializer } from '../../prismicKits';
 import { RichText } from 'prismic-reactjs';
-import { FlexRow, FlexColumn } from '../../components/containers';
+import { FlexColumn } from '../../components/containers';
 import { Input } from '../../components/inputs';
 import { PrimaryButton } from '../../components/buttons';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import { breakpoints } from '../../styles/breakpoints';
+
+const Padding = styled(FlexColumn)`
+  padding: ${props => `
+    ${props.theme.spacings.lg}
+    ${props.theme.spacings.md}
+  `}
+`
 
 const MySlice = ({ slice }) => {
   const [submitted, setSubmitted] = useState(false);
@@ -26,17 +34,17 @@ const MySlice = ({ slice }) => {
   }
 
   return (
-    <FlexColumn width='50%'>
-      <RichText render={slice.primary.text} htmlSerializer={htmlSerializer} />
+    <Padding>
+        <RichText render={slice.primary.text} htmlSerializer={htmlSerializer} />
 
-      { submitted ? (
-        <PrimaryButton as='span'>
-          Subscribed &#10004;
-        </PrimaryButton>
-      ) : (
-        <Form handleSubmit={handleSubmit} handleChange={handleChange} data={input} />
-      )}
-    </FlexColumn>
+        { submitted ? (
+          <PrimaryButton as='span'>
+            Subscribed
+          </PrimaryButton>
+        ) : (
+          <Form handleSubmit={handleSubmit} handleChange={handleChange} data={input} />
+        )}
+    </Padding>
   );
 };
 
@@ -49,8 +57,23 @@ MySlice.propTypes = {
   }).isRequired,
 };
 
+const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > *:not(:last-child) {
+    margin-bottom: ${props => props.theme.spacings.md};
+  }
+
+  @media only screen and ${breakpoints.md} {
+    width: 67%;
+  }
+`
+
 const Form = ({ handleSubmit, handleChange, data }) => (
-  <FlexColumn as='form' onSubmit={(e) => handleSubmit(e)}>
+  <StyledForm onSubmit={(e) => handleSubmit(e)}>
     <Input
       onChange={handleChange}
       value={data.email}
@@ -61,7 +84,7 @@ const Form = ({ handleSubmit, handleChange, data }) => (
     <PrimaryButton>
       Subscribe
     </PrimaryButton>
-  </FlexColumn>
+  </StyledForm>
 );
 
 Form.propTypes = {

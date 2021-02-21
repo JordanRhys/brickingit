e7e5f7a601b581cd570e865d95143e778d705af1
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 20:39:41 +0000

    added favicon to _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/globals';
 import theme from '../styles/theme';
 import { Outer } from '../components/containers';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/static/icon.ico" />
+      </Head>
       <GlobalStyle />
       <Outer>
         <Component {...pageProps} />

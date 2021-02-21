b44707f89bd43171c745e2d6eef5868d5e907648
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 19:19:18 +0000

    added styled-components next.js support

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,59 @@
+
+
+// export default class extends Document {
+//   static async getStaticProps(ctx) {
+//     const initialProps = await Document.getStaticProps(ctx);
+//     await createResolver();
+//     return { ...initialProps };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="preconnect" href="https://fonts.gstatic.com"/>
+//           <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// };
+
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { ServerStyleSheet } from 'styled-components'
 import { createResolver } from "next-slicezone/resolver";
 
-export default class extends Document {
-  static async getStaticProps(ctx) {
-    const initialProps = await Document.getStaticProps(ctx);
-    await createResolver();
-    return { ...initialProps };
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      await createResolver();
+      sheet.seal()
+    }
   }
 
   render() {
@@ -24,4 +70,4 @@ export default class extends Document {
       </Html>
     );
   }
-};
+}

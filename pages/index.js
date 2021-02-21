8c22f6479366a91ba74db48fd245599cacc19c4e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 20:33:54 +0000

    added Head data

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,15 @@ import SliceZone from "next-slicezone";
 import { useGetStaticProps } from "next-slicezone/hooks";
 
 import resolver from "../sm-resolver.js";
+import Head from "next/head";
 
-const Page = (props) => <SliceZone {...props} resolver={resolver} />;
+const Page = (props) => <>
+  <Head>
+    <title>{props.data.meta_title}</title>
+    <meta name="description" content={props.data.meta_description}/>
+  </Head>
+  <SliceZone {...props} resolver={resolver} />;
+</>
 
 // Fetch content from prismic
 export const getStaticProps = useGetStaticProps({

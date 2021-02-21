d308ef3cbcfdfaf6c1d410f089c9c13d509de8f6
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 13:40:09 +0000

    removed unused imports, added left and right padding. Implemented contrast theme.

--- slices/TestimonialCards/index.js
+++ slices/TestimonialCards/index.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import { shape, arrayOf, object, string, number } from 'prop-types';
+import React from 'react';
+import { shape, arrayOf, string } from 'prop-types';
 import { imagePropType, richTextPropType } from '../../helpers/slice-prop-types';
-import { penceToPounds } from '../../helpers/currency';
-import { FlexColumn, FlexRow, Card, FullWidth } from '../../components/containers';
+import { FlexColumn, FlexRow, Card } from '../../components/containers';
 import { htmlSerializer } from '../../prismicKits';
-import { PrimaryButton, SmallPrimaryButton, SmallSecondaryButton } from '../../components/buttons';
 import { RichText } from 'prismic-reactjs';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import contrastTheme from '../../styles/contrastTheme';
+import theme from '../../styles/theme';
 
 const Image = styled.img`
   width: 100px;
+  height: 100px;
   border-radius: 50%;
   object-fit: cover;
   margin: 0 auto;
@@ -22,25 +23,34 @@ const Name = styled.span`
   text-align: center;
 `
 
+const PaddedColumn = styled(FlexColumn)`
+  padding-left: ${props => props.theme.spacings.md};
+  padding-right: ${props => props.theme.spacings.md};
+`
+
 const MySlice = ({ slice }) => {
   const { primary, items } = slice;
 
   return (
-    <FlexColumn>
-      { primary.text && (<RichText render={primary.text} htmlSerializer={htmlSerializer} />)}
+    <ThemeProvider theme={contrastTheme}>
+      <PaddedColumn>
+        { primary.text && (<RichText render={primary.text} htmlSerializer={htmlSerializer} />)}
 
-      <FlexRow as='div'>
-        {
-          items.map(({ image, name, text }) => (
-            <Card maxWidth='25%'>
-              <Image src={image.url} alt={image.alt} />
-              <Name>{name}</Name>
-              <RichText render={text} htmlSerializer={htmlSerializer} />
-            </Card>
-          ))
-        }
-      </FlexRow>
-    </FlexColumn>
+        <FlexRow as='div'>
+          <ThemeProvider theme={theme}>
+            {
+              items.map(({ image, name, text }) => (
+                <Card maxWidth='25%'>
+                  <Image src={image.url} alt={image.alt} />
+                  <Name>{name}</Name>
+                  <RichText render={text} htmlSerializer={htmlSerializer} />
+                </Card>
+              ))
+            }
+          </ThemeProvider>
+        </FlexRow>
+      </PaddedColumn>
+    </ThemeProvider>
   );
 };
 

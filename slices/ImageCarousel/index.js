874b841d84ae33b9f110deaa2ffbce5e5833a3f2
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 12:59:13 +0000

    removed infinite-react-carousel and implemented my own

--- slices/ImageCarousel/index.js
+++ slices/ImageCarousel/index.js
@@ -3,8 +3,9 @@ import { shape, string, object, arrayOf } from 'prop-types';
 import { imagePropType } from '../../helpers/slice-prop-types';
 import { Subtle } from '../../components/typography';
 import styled from 'styled-components';
-import Slider from 'infinite-react-carousel';
 import 'infinite-react-carousel/lib/carousel/style.css';
+import { breakpoints } from '../../styles/breakpoints';
+import Carousel from '../../components/carousel';
 
 const Slide = styled.div`
   width: 100%;
@@ -23,34 +24,21 @@ const Caption = styled.span`
   justify-content: center;
 `
 
-const StyledSlider = styled(Slider)`
-  color: ${props => props.theme.colors.body};
-  .carousel-dots {
-    margin-top: ${props => props.theme.spacings.xs};
-  }
-`
-
 const MySlice = ({ slice }) => {
-  const settings = {
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 5000
-  }
 
-  return (
-      <StyledSlider {...settings}>
-        {
-          slice.items.map(({ image, caption }) => (
-            <Slide key={image.url}>
-              <Img src={image.url} alt={image.alt} />
-              <Caption><Subtle>{caption}</Subtle></Caption>
-            </Slide>
-          ))
-        }
-      </StyledSlider>
-  );
-};
+    return (
+        <Carousel>
+          {
+            slice.items.map(({ image, caption }) => (
+              <Slide key={image.url}>
+                <Img src={image.url} alt={image.alt} />
+                <Caption><Subtle>{caption}</Subtle></Caption>
+              </Slide>
+            ))
+          }
+        </Carousel>
+    );
+  };
 
 MySlice.propTypes = {
   slice: shape({

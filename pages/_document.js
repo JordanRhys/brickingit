eef5ea2b4960b438942323c7205415d00300ef49
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 18:48:19 +0000

    imported google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,10 @@ export default class extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+        </Head>
         <body>
           <Main />
           <NextScript />

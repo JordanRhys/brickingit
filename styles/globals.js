29e5c4ce2ab08fa072edd213ff8d12f8b03e1361
JordanRhysSmith <jordanrhysgtr@gmail.com>
2021-02-21 13:01:41 +0000

    removed background color

--- styles/globals.js
+++ styles/globals.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background: ${props => props.theme.colors.background};
-  }
   * {
     margin: 0;
     padding: 0;
